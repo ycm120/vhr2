f6045b133c82c0ca7c246762e08f551137a9ac10
ycm120 <18710615070@163.com>
2019-07-15 20:03:33 +0800

    init pro

---
+++ src/main/java/vhr2/Vhr2Application.java
@@ -0,0 +1,13 @@
+package vhr2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vhr2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vhr2Application.class, args);
+	}
+
+}
---
+++ src/test/java/vhr2/Vhr2ApplicationTests.java
@@ -0,0 +1,16 @@
+package vhr2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Vhr2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
