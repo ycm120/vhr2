bbd4423ad8e554569409c29c5049e919829c2dd0
ycm120 <18710615070@163.com>
2019-07-18 20:20:48 +0800

    2

--- src/main/java/vhr2/Vhr2Application.java
+++ src/main/java/vhr2/Vhr2Application.java
@@ -1,13 +1,20 @@
 package vhr2;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Vhr2Application {
+public class Vhr2Application implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Vhr2Application.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("start...");
 	}
 
 }
